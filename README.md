12448608353889bc4c3ce46ee6a653b900631c2b
Anvir  Tiwana <anvir@Anvirs-MacBook-Pro.local>
2023-09-12 14:49:46 -0400

    Read Me File

---
+++ README.md
@@ -0,0 +1,5 @@
+User Project
+
+This program allows you to read, create and delete users in the database system. The program initially starts with 3 users ; Anvir, Krystal, Nicole. 
+
+As the program runs, one user is deleted and another one is created. By the end of the program, there are 3 users with the ID 1, 3, and 4.  Therefore, user 2 is deleted. 
--- fileOperations.js
+++ fileOperations.js
@@ -1,10 +1,78 @@
 const fs = require('fs');
  
-// Read a file
-fs.readFile('example.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading the file:', err);
-        return;
+
+let users = [
+    { id: 1, name: 'Anvir' },
+    { id: 2, name: 'Krystal' },
+    { id: 3, name: 'Nicole' },
+  ];
+  
+  // Creating a new user
+  function generateUserId() {
+    const maxId = Math.max(...users.map((user) => user.id));
+    return maxId + 1;
+  }
+  
+  // Function to create a new user
+  function createUser(name) {
+    try {
+      const newUser = {
+        id: generateUserId(),
+        name,
+      };
+      users.push(newUser);
+      return newUser;
+    } catch (error) {
+      console.error('Error creating a user:', error);
+      return null;
     }
-    console.log('File content:', data);
-});
+  }
+  
+  //How to create a new user
+  const newUserName = 'Britanny';
+  const createdUser = createUser(newUserName);
+  
+  if (createdUser) {
+    console.log(`User ${createdUser.name} with ID ${createdUser.id} has been created.`);
+  } else {
+    console.log('User creation failed.');
+  }
+  
+  // Delete a user by ID:
+  function deleteUserById(userId) {
+    try {
+      // Find the index of the user with the given ID
+      const userIndex = users.findIndex((user) => user.id === userId);
+  
+      if (userIndex === -1) {
+        // There would be no users with this ID
+        return false;
+      }
+  
+      // Removing a user from the array
+      users.splice(userIndex, 1);
+      return true;
+    } catch (error) {
+      console.error('Error deleting a user:', error);
+      return false;
+    }
+  }
+  
+  // Deleting a user by ID
+  const userIdToDelete = 2;
+  const deleted = deleteUserById(userIdToDelete);
+  
+  if (deleted) {
+    console.log(`User with ID ${userIdToDelete} has been deleted.`);
+  } else {
+    console.log(`User with ID ${userIdToDelete} was not found or deletion failed.`);
+  }
+  
+  // Shows the updated list of users
+  console.log('Updated user list:');
+  for (const user of users) {
+    console.log(`User ID: ${user.id}, Name: ${user.name}`);
+  }
+  
+  
+  
