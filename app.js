5c583cdfe016118f9499af355a3dc9c30042689b
Anvir  Tiwana <anvir@Anvirs-MacBook-Pro.local>
2023-09-28 14:49:44 -0400

    Updated changes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+app.use(express.json());
+
 // Sample student data
 const students = [
   { id: 1, name: 'Mia' },
@@ -9,16 +11,118 @@ const students = [
   { id: 3, name: 'Remy' },
 ];
 
-app.use(express.json());
+// Sample user data
+let users = [
+  { id: 1, name: 'Anvir' },
+  { id: 2, name: 'Krystal' },
+  { id: 3, name: 'Nicole' },
+];
 
 // Endpoint to retrieve all students
 app.get('/api/students', (req, res) => {
   res.json(students);
 });
 
+// Endpoint to retrieve a specific student by ID
+app.get('/api/students/:id', (req, res) => {
+  const studentId = parseInt(req.params.id);
+  const student = students.find((s) => s.id === studentId);
+
+  if (!student) {
+    return res.status(404).json({ message: 'Student not found' });
+  }
+
+  res.json(student);
+});
+
+// Endpoint to create a new student
+app.post('/api/students', (req, res) => {
+  const newStudent = req.body;
+  students.push(newStudent);
+  res.status(201).json(newStudent);
+});
+
+// Endpoint to update a student by ID
+app.put('/api/students/:id', (req, res) => {
+  const studentId = parseInt(req.params.id);
+  const updatedStudent = req.body;
+  const index = students.findIndex((s) => s.id === studentId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: 'Student not found' });
+  }
+
+  students[index] = updatedStudent;
+  res.json(updatedStudent);
+});
+
+// Endpoint to delete a student by ID
+app.delete('/api/students/:id', (req, res) => {
+  const studentId = parseInt(req.params.id);
+  const index = students.findIndex((s) => s.id === studentId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: 'Student not found' });
+  }
+
+  students.splice(index, 1);
+  res.status(204).send();
+});
+
+// Endpoint to retrieve all users
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+// Endpoint to retrieve a specific user by ID
+app.get('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.json(user);
+});
+
+// Endpoint to create a new user
+app.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// Endpoint to update a user by ID
+app.put('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const updatedUser = req.body;
+  const index = users.findIndex((u) => u.id === userId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  users[index] = updatedUser;
+  res.json(updatedUser);
+});
+
+// Endpoint to delete a user by ID
+app.delete('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const index = users.findIndex((u) => u.id === userId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  users.splice(index, 1);
+  res.status(204).send();
+});
+
 // Default route for handling the root path
 app.get('/', (req, res) => {
-  res.send('Welcome to the Student API');
+  res.send('Welcome to the Student and User API');
 });
 
 // Start the server
--- fileOperations.js
+++ fileOperations.js
@@ -1,118 +1,108 @@
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
-const { readFile } = require('fs/promises');
+const { readFile, writeFile } = require('fs/promises');
 const express = require('express');
-const bodyParser = require('body-parser');
-
-
 
 let users = [
-    { id: 1, name: 'Anvir' },
-    { id: 2, name: 'Krystal' },
-    { id: 3, name: 'Nicole' },
-  ];
-  
-  //Read a file using node fs
-  fs.readFile(path.join(__dirname, 'file.txt'), 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log("\nFile 1 Contents:");
-    console.log(data);
-  });
+  { id: 1, name: 'Anvir' },
+  { id: 2, name: 'Krystal' },
+  { id: 3, name: 'Nicole' },
+];
 
-  //Writing in a file using node fs
-  const filePath = "fileTwo.txt"
-  const contentToWrite = "Goodbye World!"
+// Read a file using node fs
+fs.readFile(path.join(__dirname, 'file.txt'), 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log("\nFile 1 Contents:");
+  console.log(data);
+});
 
-  fs.writeFile(filePath, contentToWrite,(err) =>{
-    if (err){
-      console.error("Error writing into the file:", err);
-    }else{
-      console.log("\nContent has been added to file 2!");
-      console.log("\nUpdated Content in File 2:")
-      console.log(contentToWrite); // displays the updated content in file 2
-    }
-  } )
+// Writing in a file using node fs
+const filePath = "fileTwo.txt"
+const contentToWrite = "Goodbye World!"
 
-  // Create a server using HTTP Module, displays a page on http://localhost:3000/ that says "Welcome to my website"
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Welcome to my Website")
-    res.end();
-  }).listen(3000);
-  
-  // REST APIs, 
-
-
-  
-
-  // Creating a new user
-  function generateUserId() {
-    const maxId = Math.max(...users.map((user) => user.id));
-    return maxId + 1;
-  }
-  
-  // Function to create a new user
-  function createUser(name) {
-    try {
-      const newUser = {
-        id: generateUserId(),
-        name,
-      };
-      users.push(newUser);
-      return newUser;
-    } catch (error) {
-      console.error('Error creating a user:', error);
-      return null;
-    }
-  }
-  
-  //How to create a new user
-  const newUserName = 'Britanny';
-  const createdUser = createUser(newUserName);
-  
-  if (createdUser) {
-    console.log(`User ${createdUser.name} with ID ${createdUser.id} has been created.`);
+fs.writeFile(filePath, contentToWrite, (err) => {
+  if (err) {
+    console.error("Error writing into the file:", err);
   } else {
-    console.log('User creation failed.');
+    console.log("\nContent has been added to file 2!");
+    console.log("\nUpdated Content in File 2:")
+    console.log(contentToWrite); // displays the updated content in file 2
   }
-  
-  // Delete a user by ID:
-  function deleteUserById(userId) {
-    try {
-      // Find the index of the user with the given ID
-      const userIndex = users.findIndex((user) => user.id === userId);
-  
-      if (userIndex === -1) {
-        // There would be no users with this ID
-        return false;
-      }
-  
-      // Removing a user from the array
-      users.splice(userIndex, 1);
-      return true;
-    } catch (error) {
-      console.error('Error deleting a user:', error);
+});
+
+// Create a server using HTTP Module, displays a page on http://localhost:3000/ that says "Welcome to my website"
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("Welcome to my Website")
+  res.end();
+}).listen(3000);
+
+// REST APIs, ...
+
+// Creating a new user
+function generateUserId() {
+  const maxId = Math.max(...users.map((user) => user.id));
+  return maxId + 1;
+}
+
+// Function to create a new user
+function createUser(name) {
+  try {
+    const newUser = {
+      id: generateUserId(),
+      name,
+    };
+    users.push(newUser);
+    return newUser;
+  } catch (error) {
+    console.error('Error creating a user:', error);
+    return null;
+  }
+}
+
+// How to create a new user
+const newUserName = 'Britanny';
+const createdUser = createUser(newUserName);
+
+if (createdUser) {
+  console.log(`User ${createdUser.name} with ID ${createdUser.id} has been created.`);
+} else {
+  console.log('User creation failed.');
+}
+
+// Delete a user by ID:
+function deleteUserById(userId) {
+  try {
+    // Find the index of the user with the given ID
+    const userIndex = users.findIndex((user) => user.id === userId);
+
+    if (userIndex === -1) {
+      // There would be no users with this ID
       return false;
     }
+
+    // Removing a user from the array
+    users.splice(userIndex, 1);
+    return true;
+  } catch (error) {
+    console.error('Error deleting a user:', error);
+    return false;
   }
-  
-  // Deleting a user by ID
-  const userIdToDelete = 2;
-  const deleted = deleteUserById(userIdToDelete);
-  
-  if (deleted) {
-    console.log(`User with ID ${userIdToDelete} has been deleted.`);
-  } else {
-    console.log(`User with ID ${userIdToDelete} was not found or deletion failed.`);
-  }
-  
-  // Shows the updated list of users
-  console.log('Updated user list:');
-  for (const user of users) {
-    console.log(`User ID: ${user.id}, Name: ${user.name}`);
-  }
-  
-  
-  
-  
+}
+
+// Deleting a user by ID
+const userIdToDelete = 2;
+const deleted = deleteUserById(userIdToDelete);
+
+if (deleted) {
+  console.log(`User with ID ${userIdToDelete} has been deleted.`);
+} else {
+  console.log(`User with ID ${userIdToDelete} was not found or deletion failed.`);
+}
+
+// Shows the updated list of users
+console.log('Updated user list:');
+for (const user of users) {
+  console.log(`User ID: ${user.id}, Name: ${user.name}`);
+}
