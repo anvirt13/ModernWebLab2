8d93cc3c279f88e72fd39f0ad4114daece05bb52
Anvir  Tiwana <anvir@Anvirs-MacBook-Pro.local>
2023-10-03 14:11:08 -0400

    Lab 4 reqs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const EventEmitter = require('events');
+const customEmitter = new EventEmitter();
+
 // Sample student data
 const students = [
   { id: 1, name: 'Mia', grade: 'B' },
@@ -16,54 +19,95 @@ const students = [
 
 app.use(express.json());
 
-// Version 1 of the API
-app.get('/api/v1/students', (req, res) => {
+// Helper function to filter students
+async function filterStudents(query) {
   try {
     let filteredStudents = [...students];
 
-    // Filtering by grade
-    const gradeFilter = req.query.grade;
-    if (gradeFilter) {
-      filteredStudents = filteredStudents.filter(student => student.grade === gradeFilter);
+    if (query.grade) {
+      filteredStudents = filteredStudents.filter(student => student.grade === query.grade);
     }
 
-    // Sorting by name
-    const sortField = req.query.sort;
-    if (sortField === 'name') {
+    if (query.sort === 'name') {
       filteredStudents.sort((a, b) => a.name.localeCompare(b.name));
-    } else if (sortField === 'grade') {
+    } else if (query.sort === 'grade') {
       filteredStudents.sort((a, b) => a.grade.localeCompare(b.grade));
     }
 
-    // Pagination
+    return filteredStudents;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Helper function to paginate students
+function paginateStudents(data, page, pageSize) {
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  return data.slice(startIndex, endIndex);
+}
+
+// Version 1 of the API
+app.get('/api/v1/students', async (req, res, next) => {
+  try {
+    const query = {
+      grade: req.query.grade,
+      sort: req.query.sort,
+    };
+
+    const filteredStudents = await filterStudents(query);
+
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.pageSize) || 10;
-    const startIndex = (page - 1) * pageSize;
-    const endIndex = startIndex + pageSize;
-    const paginatedStudents = filteredStudents.slice(startIndex, endIndex);
+    const paginatedStudents = paginateStudents(filteredStudents, page, pageSize);
 
-    // Checks if the requested page is out of range
-    if (page < 1 || startIndex >= filteredStudents.length) {
-      throw new Error('Page not found');
+    if (page < 1 || paginatedStudents.length === 0) {
+      const error = new Error('Page not found');
+      error.statusCode = 404;
+      throw error;
     }
 
     res.json(paginatedStudents);
   } catch (error) {
-    // Handle errors
-    res.status(400).json({ error: error.message });
+    next(error); // Pass the error to Express's error handling middleware
   }
 });
 
+// Custom event example
+app.get('/custom-event', (req, res, next) => {
+  try {
+    // Emit a custom event
+    customEmitter.emit('customEvent', 'Custom event was triggered');
+    res.send('Custom event emitted.');
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Listen for the custom event
+customEmitter.on('customEvent', (message) => {
+  console.log(message);
+});
+
 // Get the HTML file
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res, next) => {
+  try {
+    res.sendFile(__dirname + '/index.html');
+  } catch (error) {
+    next(error);
+  }
 });
 app.use(express.static(__dirname));
-
 
 // Default route for handling the root path
 app.get('/', (req, res) => {
   res.send('Welcome to the Student API');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).json({ error: err.message });
 });
 
 // Start the server
