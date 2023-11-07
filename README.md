61bd36eb97ebf83dc9c89915ba822bdb837ca8c9
Anvir  Tiwana <anvir@Anvirs-MacBook-Pro.local>
2023-11-07 12:15:40 -0500

    lab 6

--- README.md
+++ README.md
@@ -8,9 +8,11 @@ Features:
 - The CRUD portion of the app allows you to choose an existing .txt file from your computer and edit it/view it's contents. It also allows you to create a brand new app that you can name and once created, it shows the files in the "All Files" section where you can edit & delete them. 
 
 Code Structure:
-- App.js: The main React component for student data management.
-- CrudApp.js: A React component for text file CRUD operations.
-- fileOperations.js: The Express.js server for serving API endpoints and handling text file operations.
+- App.js: This is the main React component responsible for managing student data and displaying the user interface. It includes features for adding students and sorting the student list.
+
+- CrudApp.js: A React component that facilitates CRUD (Create, Read, Update, Delete) operations on text files. Users can create, edit, save, and delete text files. This component uses a custom hook called useTextFileManagement to manage file-related operations and state.
+
+- fileOperations.js: This file sets up an Express.js server to serve API endpoints and manage text file operations. It handles routes for serving and managing text files and provides an API for student data. Error handling is also incorporated.
 
 CodeClimate Summary: 
 - Maintaiability: B
--- rApp/apps/src/crudApp.js
+++ rApp/apps/src/crudApp.js
@@ -1,132 +1,69 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
+import { useTextFileManagement } from './useTextFileManagement'; // Import the custom hook
 
-class CrudApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      files: [],
-      selectedFile: null,
-      fileContent: '',
-      editedText: '',
-      isEditing: false,
-      newFileName: '',
-    };
-  }
+function CrudApp() {
+  const {
+    files,
+    selectedFile,
+    fileContent,
+    editedText,
+    isEditing,
+    newFileName,
+    handleFileChange,
+    createFile,
+    selectFile,
+    editFile,
+    saveFile,
+    deleteFile,
+    setNewFileName,
+    setEditedText,
+  } = useTextFileManagement(); // Custom hook
 
-  fileInputRef = React.createRef();
+  const fileInputRef = useRef();
 
-  handleFileChange = () => {
-    const file = this.fileInputRef.current.files[0];
-    if (file) {
-      const reader = new FileReader();
+  return (
+    <div>
+      <h1>CRUD System for Text Files</h1>
+      <input type="file" accept=".txt" ref={fileInputRef} onChange={() => handleFileChange(fileInputRef)} />
 
-      reader.onload = (event) => {
-        const content = event.target.result;
-        this.setState({ fileContent: content });
-      };
-
-      reader.readAsText(file);
-    }
-  };
-
-  handleCreateFile = () => {
-    const { newFileName, files } = this.state;
-    if (newFileName) {
-      const newFile = {
-        name: newFileName + '.txt',
-        content: '',
-      };
-      this.setState({
-        files: [...files, newFile],
-        newFileName: '',
-      });
-    }
-  };
-
-  handleFileSelect = (file) => {
-    this.setState({
-      selectedFile: file,
-      fileContent: file.content,
-      isEditing: false,
-    });
-  };
-
-  handleFileEdit = () => {
-    this.setState({ isEditing: true });
-  };
-
-  handleFileSave = () => {
-    const { files, selectedFile, editedText } = this.state;
-    const updatedFiles = files.map((file) => {
-      if (file === selectedFile) {
-        return { ...file, content: editedText };
-      }
-      return file;
-    });
-    this.setState({
-      files: updatedFiles,
-      isEditing: false,
-    });
-  };
-
-  handleFileDelete = () => {
-    const { files, selectedFile } = this.state;
-    const updatedFiles = files.filter((file) => file !== selectedFile);
-    this.setState({
-      files: updatedFiles,
-      selectedFile: null,
-    });
-  };
-
-  render() {
-    const { files, selectedFile, fileContent, editedText, isEditing, newFileName } = this.state;
-
-    return (
+      <h2>Create New File:</h2>
       <div>
-        <h1>CRUD System for Text Files</h1>
-        <input type="file" accept=".txt" ref={this.fileInputRef} onChange={this.handleFileChange} />
-
-        <h2>Create New File:</h2>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter file name"
-            value={newFileName}
-            onChange={(e) => this.setState({ newFileName: e.target.value })}
-          />
-          <button onClick={this.handleCreateFile}>Create</button>
-        </div>
-        
-        <div>
-          <h2>Selected File: {selectedFile ? selectedFile.name : 'No File Selected'}</h2>
-          {isEditing ? (
-            <div>
-              <textarea value={editedText} onChange={(e) => this.setState({ editedText: e.target.value })} />
-              <button onClick={this.handleFileSave}>Save</button>
-            </div>
-          ) : (
-            <div>
-              <div>Content:</div>
-              <pre>{fileContent}</pre>
-              <button onClick={this.handleFileEdit}>Edit</button>
-              <button onClick={this.handleFileDelete}>Delete</button>
-            </div>
-          )}
-        </div>
-
-       
-
-        <h2>All Files:</h2>
-        <ul>
-          {files.map((file, index) => (
-            <li key={index} onClick={() => this.handleFileSelect(file)}>
-              {file.name}
-            </li>
-          ))}
-        </ul>
+        <input
+          type="text"
+          placeholder="Enter file name"
+          value={newFileName}
+          onChange={(e) => setNewFileName(e.target.value)}
+        />
+        <button onClick={createFile}>Create</button>
       </div>
-    );
-  }
+
+      <div>
+        <h2>Selected File: {selectedFile ? selectedFile.name : 'No File Selected'}</h2>
+        {isEditing ? (
+          <div>
+            <textarea value={editedText} onChange={(e) => setEditedText(e.target.value)} />
+            <button onClick={saveFile}>Save</button>
+          </div>
+        ) : (
+          <div>
+            <div>Content:</div>
+            <pre>{fileContent}</pre>
+            <button onClick={editFile}>Edit</button>
+            <button onClick={deleteFile}>Delete</button>
+          </div>
+        )}
+      </div>
+
+      <h2>All Files:</h2>
+      <ul>
+        {files.map((file, index) => (
+          <li key={index} onClick={() => selectFile(file)}>
+            {file.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default CrudApp;
